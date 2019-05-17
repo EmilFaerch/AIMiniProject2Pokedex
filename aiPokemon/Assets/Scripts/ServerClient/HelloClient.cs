339d482f6515874c8fc5c80dc7bf97c5178b9289
Emil <no>
2019-05-17 09:30:10 +0200

    uiyk

---
+++ aiPokemon/Assets/Scripts/ServerClient/HelloClient.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+public class HelloClient : MonoBehaviour
+{
+    private HelloRequester _helloRequester;
+    public bool SendPack = true;
+    private byte[] current_img;
+
+    public void SendObject(string msg)
+    {    
+        _helloRequester.bytes = Encoding.ASCII.GetBytes(msg);
+        _helloRequester.Continue();
+    }
+
+
+    private void OnPostRender()
+    {
+        /*
+        if (SendPack)
+        {
+            string hey = "Hello world!";
+            _helloRequester.bytes = Encoding.ASCII.GetBytes(hey);
+            _helloRequester.Continue();
+        }
+        else if (!SendPack)
+        {
+            _helloRequester.Pause();
+
+        }
+        */
+    }
+
+    private void Start()
+    {   
+        _helloRequester = new HelloRequester();
+        Debug.Log("HelloClient: helloRequester initialized.");
+        _helloRequester.Start();
+    }
+
+    private void OnDestroy()
+    {
+        _helloRequester.Stop();
+    }
+
+    private void OnApplicationQuit()
+    {
+        _helloRequester.Stop();
+    }
+}
---
+++ aiPokemon/Assets/Scripts/ServerClient/HelloRequester.cs
@@ -0,0 +1,62 @@
+﻿using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using Newtonsoft.Json;
+
+///     You can copy this class and modify Run() to suits your needs.
+///     To use this class, you just instantiate, call Start() when you want to start and Stop() when you want to stop.
+
+public class HelloRequester : RunAbleThread
+{
+
+    public GameObject Pokedex;
+
+    public byte[] bytes;
+    ///     Stop requesting when Running=false.
+    protected override void Run()
+    {
+        ForceDotNet.Force();
+
+        using (RequestSocket client = new RequestSocket())
+        {
+            client.Connect("tcp://localhost:5555");
+
+            Debug.Log("Client Connected");
+
+            while (Running)
+            {
+                Debug.Log("Server Running ...");
+
+                if (Send)
+                {
+                    //string message = client.ReceiveFrameString();
+                    client.SendFrame(bytes);
+                    string message = null;
+                    bool gotMessage = false;
+
+                    while (Running)
+                    {
+                        Debug.Log("Server Listening ...");
+                        gotMessage = client.TryReceiveFrameString(out message); // this returns true if it's successful
+                        if (gotMessage) break;
+                    }
+
+                    if (gotMessage)
+                    {
+                        Debug.Log("Received: " + message);
+
+
+
+                        Send = false;
+                    }
+                }
+
+                NetMQConfig.Cleanup();
+            }
+        }
+    }
+}
